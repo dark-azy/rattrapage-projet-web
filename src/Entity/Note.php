0e8379afd28f4bc2197022826a6577257f655293
Majcher <vladimir.majcher@viacesi.fr>
2025-04-29 14:34:04 +0200

    création de la notation des entreprise par les élèves

---
+++ src/Entity/Note.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NoteRepository::class)]
+#[ORM\Table(name: 'note')]
+#[ORM\UniqueConstraint(name: 'unique_etudiant_entreprise', columns: ['nom_etudiant', 'nom_entreprise'])]
+class Note
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomEtudiant = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomEntreprise = null;
+
+    #[ORM\Column]
+    private ?int $valeur = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEtudiant(): ?string
+    {
+        return $this->nomEtudiant;
+    }
+
+    public function setNomEtudiant(string $nomEtudiant): self
+    {
+        $this->nomEtudiant = $nomEtudiant;
+        return $this;
+    }
+
+    public function getNomEntreprise(): ?string
+    {
+        return $this->nomEntreprise;
+    }
+
+    public function setNomEntreprise(string $nomEntreprise): self
+    {
+        $this->nomEntreprise = $nomEntreprise;
+        return $this;
+    }
+
+    public function getValeur(): ?int
+    {
+        return $this->valeur;
+    }
+
+    public function setValeur(int $valeur): self
+    {
+        if ($valeur < 1 || $valeur > 5) {
+            throw new \InvalidArgumentException('La note doit être comprise entre 1 et 5');
+        }
+        $this->valeur = $valeur;
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+        return $this;
+    }
+} 
